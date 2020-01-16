752a6f4a3c24889de23d5d12e1f90fc9d1f5edb3
Infocat <infocat2.0@gmail.com>
2020-01-16 18:22:49 -0500

    0.6g

--- resources/views/plantillas/inicio.blade.php
+++ resources/views/plantillas/inicio.blade.php
@@ -1,9 +1,10 @@
 @extends('plantillas.panel')
 
 @section('titulo')
-<h2><i class="icofont-ui-home"></i> Inicio</h2>
+<h2><i class="icofont-industries-2"></i> Intranet para Fábrica Marie</h2>
 @endsection
 
 @section('cuerpo')
-<p>Bienvenido al sistema de Fábrica Marie, seleccione el menú y empiece a navegar</p>
+<img src="{{url('images/bannersimple.jpg')}}" alt="">
+<h5>Bienvenido, seleccione el menú y empiece a navegar</h5>
 @endsection
