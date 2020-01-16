5a461b57f008f3d06bfd6b90687a7882a571d1f2
Infocat <infocat2.0@gmail.com>
2020-01-16 12:10:14 -0500

    0.6d

--- resources/views/plantillas/menu.blade.php
+++ resources/views/plantillas/menu.blade.php
@@ -9,7 +9,7 @@
 	</div>
 
 	<ul class="list-unstyled components">
-		<p class="text-center"><small>Versión 1.0.6</small></p>
+		<p class="text-center"><small>Versión 1.0.6d</small></p>
 		<li>
 			<a href="{{route('panel')}}" class="d-flex align-items-center"><i class="icofont-home"></i> <span class="liText">Principal</span> </a>
     </li>
